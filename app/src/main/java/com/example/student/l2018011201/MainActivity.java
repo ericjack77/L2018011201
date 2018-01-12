ef745af68740f37c392434a0b97e4c49e04668a8
ericchiu <ericjack7@gmail.com>
2018-01-12 10:10:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/l2018011201/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.student.l2018011201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            JSONObject obj0 = array.getJSONObject(0);
+                            String district = obj0.getString("district");
+                            Log.d("district", district);
+                            String address = obj0.getString("address");
+                            Log.d("address", address);
+                            String tel = obj0.getString("tel");
+                            Log.d("tel", tel);
+                            String opening_hours = obj0.getString("opening_hours");
+                            Log.d("opening_hours",opening_hours);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+    }
+}
