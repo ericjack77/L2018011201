7a359d2d9bb56cb263be7a9c906fee38d26f6b04
ericchiu <ericjack7@gmail.com>
2018-01-12 11:09:17 +0800

    2. Gson法

---
+++ app/src/main/java/com/example/student/l2018011201/Animal.java
@@ -0,0 +1,12 @@
+package com.example.student.l2018011201;
+
+/**
+ * Created by Student on 2018/1/12.
+ */
+
+public class Animal {
+    String district;
+    String address;
+    String tel;
+    String opening_hours;
+}
